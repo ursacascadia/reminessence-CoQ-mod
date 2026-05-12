d6a26eee3bd37a4df4a35ebee13f5fad69c1d228
Cassie <cjlane204@gmail.com>
2026-05-11 17:55:45 -0700

    show popup, close ui when used from inventory

--- Scripts/Parts/Memorum_Crystal.cs
+++ Scripts/Parts/Memorum_Crystal.cs
@@ -12,19 +12,31 @@ namespace XRL.World.Parts {
 
         public override bool WantEvent(int ID, int cascade)
         {
-            if (!base.WantEvent(ID, cascade) && ID != GetInventoryActionsEvent.ID)
-            {
-                return ID == InventoryActionEvent.ID;
+            if (ID == GetInventoryActionsEvent.ID){
+                return true;
             }
-            // else if (!base.WantEvent(ID, cascade) && ID != DeathEvent.ID) {
-            //     return ID == DeathEvent.ID;
-            // }
-            return true;
+            if (ID == InventoryActionEvent.ID)
+            {
+                return true;
+            }
+            if (ID == BeforeDieEvent.ID)
+            {
+                return true;
+            }
+            if (ID == TakenEvent.ID)
+            {
+                return true;
+            }
+            if (ID == DroppedEvent.ID)
+            {
+                return true;
+            }
+            return base.WantEvent(ID, cascade);
         }
 
         public override bool HandleEvent(GetInventoryActionsEvent E)
         {
-            E.AddAction("Crush", "crush", COMMAND_NAME, null, 'C', FireOnActor: false, 10);
+            E.AddAction("Crush", "Crush", COMMAND_NAME, null, 'C', FireOnActor: false, 10);
             return base.HandleEvent(E);
         }
 
@@ -33,7 +45,20 @@ namespace XRL.World.Parts {
             if (E.Command == COMMAND_NAME)
             {
                 CrushCrystal();
+                E.RequestInterfaceExit();
             }
+            return base.HandleEvent(E);
+        }
+
+        public override bool HandleEvent(TakenEvent E)
+        {
+            E.Actor.RegisterEvent(this, BeforeDieEvent.ID);
+            return base.HandleEvent(E);
+        }
+
+        public override bool HandleEvent(DroppedEvent E)
+        {
+            E.Actor.UnregisterEvent(this, BeforeDieEvent.ID);
             return base.HandleEvent(E);
         }
 
@@ -41,22 +66,40 @@ namespace XRL.World.Parts {
         {
             return true;
         }
+        
+        // public override void Register(GameObject Object, IEventRegistrar Registrar)
+        // {
+        //     Registrar.Register("BeforeDie");
+        //     base.Register(Object, Registrar);
+        // }
+
+        // public override bool FireEvent(Event E)
+        // {
+        //     if (E.ID == "BeforeDie")
+        //     {
+        //         return CrushCrystal();
+        //     }
+        //     return base.FireEvent(E);
+        // }
+
+        public override bool HandleEvent(BeforeDieEvent E)
+        {
+            // XRL.Messages.MessageQueue.AddPlayerMessage("[Debug: Died, so activating crystal.]");
+            CrushCrystal();
+            return false;
+        }
 
         public bool CrushCrystal()
         {
+            // XRL.Messages.MessageQueue.AddPlayerMessage("[Debug: Crystal crush attempt.]");
             // PlayWorldSound("Sounds/Interact/sfx_interact_curlingIron_press");
             if (XRL.XRLGame.LoadCurrentGame("Crystal") == null)
             {
-                XRL.Messages.MessageQueue.AddPlayerMessage("[Debug: Command failed.]");
+                // XRL.Messages.MessageQueue.AddPlayerMessage("[Debug: Command failed.]");
                 return false;
             }
+            XRL.UI.Popup.Show("The crystal disentegrates. Reminiscense flashes before your eyes.");
             return true;
         }
-
-        // public override bool HandleEvent(DeathEvent E)
-        // {
-        //     CrushCrystal();
-        //     return base.HandleEvent(E);
-        // }
     }
 }
